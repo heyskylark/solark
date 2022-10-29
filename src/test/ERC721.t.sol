4a613394d5c07d2f73dcb80208074f301ade25ae
Brandon Feist <brandon.feist@hey.com>
2022-10-29 13:42:35 -0700

    forge test init

---
+++ src/test/ERC721.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.13;
+
+import "ds-test/test.sol";
+
+contract ERC721Test is DSTest {
+  function setUp() public {}
+
+  function testExample() public {
+      assertTrue(true);
+  }
+}
