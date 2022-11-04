55500c42d10c12d4a51e40d5c7dd11cf70dd70bb
Brandon Feist <brandon.feist@hey.com>
2022-11-03 18:30:31 -0700

    EIP-2612 impl on ERC20 contract

--- src/tokens/ERC20.sol
+++ src/tokens/ERC20.sol
@@ -10,6 +10,14 @@ abstract contract ERC20 {
                       EVENTS
   //////////////////////////////////////////////*/
 
+  error ExpiredPermit();
+
+  error InvalidSigner();
+
+  /*//////////////////////////////////////////////
+                      EVENTS
+  //////////////////////////////////////////////*/
+
   event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
   event Approval(address indexed _owner, address indexed _spender, uint256 _value);
@@ -35,6 +43,16 @@ abstract contract ERC20 {
   mapping(address => mapping(address => uint256)) public allowance;
 
   /*//////////////////////////////////////////////
+                  EIP-2612 STORAGE
+  //////////////////////////////////////////////*/
+
+  uint256 internal immutable INIT_CHAIN_ID;
+
+  bytes32 internal immutable INIT_DOMAIN_SEPERATOR;
+
+  mapping(address => uint256) public nonces;
+
+  /*//////////////////////////////////////////////
                     CONSTRUCTOR
   //////////////////////////////////////////////*/
 
@@ -46,6 +64,75 @@ abstract contract ERC20 {
     name = _name;
     symbol = _symbol;
     decimals = _decimals;
+
+    INIT_CHAIN_ID = block.chainid;
+    INIT_DOMAIN_SEPERATOR = computeDomainSeperator();
+  }
+
+  /*//////////////////////////////////////////////
+                  EIP-2612 LOGIC
+  //////////////////////////////////////////////*/
+
+  function permit(
+    address owner,
+    address spender,
+    uint256 value,
+    uint256 deadline,
+    uint8 v,
+    bytes32 r,
+    bytes32 s
+  ) public virtual {
+    if (deadline < block.timestamp) {
+      revert ExpiredPermit();
+    }
+
+    unchecked {
+      address recoveredAddress = ecrecover(
+        keccak256(
+          abi.encode(
+            "\x19\x01",
+            DOMAIN_SEPARATOR(),
+            keccak256(
+              abi.encode(
+                keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"),
+                owner,
+                spender,
+                value,
+                nonces[owner]++,
+                deadline
+              )
+            )
+          )
+        ),
+        v,
+        r,
+        s
+      );
+
+      if (recoveredAddress == address(0) || recoveredAddress != owner) {
+        revert InvalidSigner();
+      }
+
+      allowance[owner][spender] += value;
+    }
+
+    emit Approval(owner, spender, value);
+  }
+
+  function DOMAIN_SEPARATOR() public view virtual returns (bytes32) {
+    return block.chainid == INIT_CHAIN_ID ? INIT_DOMAIN_SEPERATOR : computeDomainSeperator();
+  }
+
+  function computeDomainSeperator() public view virtual returns (bytes32) {
+    return keccak256(
+      abi.encode(
+        keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)'),
+        keccak256(bytes(name)),
+        keccak256("1"),
+        block.chainid,
+        address(this)
+      )
+    );
   }
 
   /*//////////////////////////////////////////////
